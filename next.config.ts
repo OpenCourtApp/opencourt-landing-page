26d01920107dfa3ae4aa229aa367b8484c0b23cd
Breno Valentim <brenosuvale@gmail.com>
2026-08-31 13:52:02 -0300

    Update next.config.ts for static export settings
    Configured Next.js for static export and GitHub Pages compatibility.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export', // Ativa a exportação estática (gera a pasta 'out')
+  images: {
+    unoptimized: true, // Obrigatório: o GitHub Pages não suporta a otimização de imagens padrão do Next
+  },
+  basePath: "/opencourt-landing-page",
+};
 
 export default nextConfig;
